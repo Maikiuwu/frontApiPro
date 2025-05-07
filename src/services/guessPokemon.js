044fe5d25b4a1bad1a082fc190019c2e6b25492f
CEhM <ceduardo.eh@gmail.com>
2025-05-07 00:40:26 -0500

    feat: integrate Tailwind CSS and JS Confetti, refactor Pokémon guessing game
    - Added Tailwind CSS and JS Confetti as dependencies.
    - Removed old CSS and App component, replacing it with a new structure for the Pokémon guessing game.
    - Created a new App component in `src/app/pages/guessThePokemon.jsx` with improved UI and functionality.
    - Implemented guessing logic with error handling and success feedback using confetti.
    - Updated Vite configuration to include Tailwind CSS plugin.

--- src/services/guessPokemon.js
+++ src/services/guessPokemon.js
@@ -1,7 +1,7 @@
 async function guessPokemon(name) {
     const resp = await fetch(`http://localhost:3000/guess/${name}`);
     if (!resp.ok) throw new Error('Error guess Pokemon');
-    return resp.json(); // aquí ya obtienes { id, name, image, types }
+    return resp.json(); // aquí ya obtienes {"correct": false,"actual": "ivysaur"}
   }
   
   export default guessPokemon
